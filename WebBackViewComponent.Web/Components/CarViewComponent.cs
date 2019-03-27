25b857ee53388c064d5fb04d8be39d68f76dee17
Lars <lars@k7c.dk>
2019-03-27 19:52:50 +0100

    fixed invoke

--- WebBackViewComponent.Web/Components/CarViewComponent.cs
+++ WebBackViewComponent.Web/Components/CarViewComponent.cs
@@ -1,11 +1,10 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using WebBackViewComponent.Web.Models;
 
 namespace WebBackViewComponent.Web.Components
 {
     public class CarViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync(CarViewModel model) => View(model);
+        public IViewComponentResult Invoke(CarViewModel model) => View(model);
     }
 }
